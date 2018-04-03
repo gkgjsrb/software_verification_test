c6075df31766daf06eea1096bb23e7037bd8ef5e
gkgjsrb <gkgjsrb@gmail.com>
2018-04-03 15:52:00 +0900

    first commit

---
+++ src/main/java/Sum.java
@@ -0,0 +1,24 @@
+public class Sum {
+
+    public int sum(int sum1, int sum2) {
+        return sum1 + sum2;
+    }
+
+    public int min(int min1, int min2) {
+        return min1 - min2;
+    }
+
+    public int mul(int mul1, int mul2) {
+        return mul1 * mul2;
+    }
+
+    public int div(int div1, int div2) {
+        return div1 / div2;
+    }
+
+
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+	int a = sum.sum(3, 4);
+    }
+}
---
+++ src/test/java/SumTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@RunWith(SerenityRunner.class)
+public class SumTest {
+    private Sum sum;
+
+    @Before
+    public void setUp() throws Exception {
+        sum = new Sum();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.sum = null;
+    }
+
+    @Test
+    public void sum() {
+        assertEquals(this.sum.sum(3, 5), 8);
+    }
+
+    @Test
+    public void min() {
+        assertEquals(this.sum.min(5, 3), 2);
+    }
+
+    @Test
+    public void mul() {
+        assertEquals(this.sum.mul(3, 5), 15);
+    }
+
+    @Test
+    public void div() {
+        assertEquals(this.sum.div(15, 5), 3);
+    }
+}
